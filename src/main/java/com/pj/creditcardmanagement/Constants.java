ec53bf9edabb2c8754e15c4bc0bdbefe76211a66
PJ Miranda <pendelhaven3@gmail.com>
2015-04-24 19:50:20 +0800

    Added totals fields in Credit Card Transactions list screen

--- src/main/java/com/pj/creditcardmanagement/Constants.java
+++ src/main/java/com/pj/creditcardmanagement/Constants.java
@@ -1,8 +1,11 @@
 package com.pj.creditcardmanagement;
 
+import java.math.BigDecimal;
+
 public class Constants {
 
 	public static final String AMOUNT_FORMAT = "#,##0.00";
 	public static final String DATE_FORMAT = "MM/dd/yyyy";
+	public static final BigDecimal ZERO = BigDecimal.ZERO.setScale(2);
 	
 }
--- src/main/java/com/pj/creditcardmanagement/screen/CreditCardTransactionsListScreen.java
+++ src/main/java/com/pj/creditcardmanagement/screen/CreditCardTransactionsListScreen.java
@@ -1,13 +1,18 @@
 package com.pj.creditcardmanagement.screen;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
@@ -20,13 +25,13 @@ import javafx.util.Callback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.pj.creditcardmanagement.Constants;
 import com.pj.creditcardmanagement.dialog.FilterCreditCardTransactionsDialog;
 import com.pj.creditcardmanagement.model.CreditCard;
 import com.pj.creditcardmanagement.model.CreditCardTransaction;
 import com.pj.creditcardmanagement.model.CreditCardTransactionSearchCriteria;
 import com.pj.creditcardmanagement.service.CreditCardTransactionService;
 import com.pj.creditcardmanagement.util.FormatterUtil;
-import com.sun.javafx.collections.ObservableListWrapper;
 
 /**
  * 
@@ -42,9 +47,13 @@ public class CreditCardTransactionsListScreen extends StandardScreen {
 	private TableView<CreditCardTransaction> table;
 	private Button addButton = new Button("Add");
 	private Button filterButton = new Button("Filter");
+	private Label totalItemsField;
+	private Label totalAmountField;
 
 	@Override
 	public void layoutComponents(GridPane grid) {
+		grid.setVgap(10);
+		
 		table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 
 		TableColumn<CreditCardTransaction, CreditCard> creditCardColumn = new TableColumn<>("Credit Card");
@@ -78,8 +87,32 @@ public class CreditCardTransactionsListScreen extends StandardScreen {
 
 		grid.add(table, 0, 0);
 		
-		GridPane.setHgrow(table, Priority.SOMETIMES);
-		GridPane.setVgrow(table, Priority.SOMETIMES);
+		GridPane.setHgrow(table, Priority.ALWAYS);
+		GridPane.setVgrow(table, Priority.ALWAYS);
+		
+		GridPane totalsGrid = createTotalsGrid();
+		grid.add(totalsGrid, 0, 1);
+	}
+
+	private GridPane createTotalsGrid() {
+		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(0, 5, 5, 0));
+		grid.setAlignment(Pos.CENTER_RIGHT);
+		
+		grid.add(new Label("Total Items"), 0, 0);
+		grid.add(totalItemsField, 1, 0);
+		grid.add(new Label("Total Amount"), 0, 1);
+		grid.add(totalAmountField, 1, 1);
+		
+		totalItemsField.setMinWidth(100);
+		totalItemsField.setAlignment(Pos.CENTER_RIGHT);
+		
+		totalAmountField.setMinWidth(100);
+		totalAmountField.setAlignment(Pos.CENTER_RIGHT);
+		
+		return grid;
 	}
 
 	@Override
@@ -92,12 +125,28 @@ public class CreditCardTransactionsListScreen extends StandardScreen {
 				getScreenController().showUpdateCreditCardTransactionScreen(table.getSelectionModel().getSelectedItem());
 			}
 		});
+		
+		totalItemsField = new Label();
+		totalAmountField = new Label();
 	}
 
 	public void updateDisplay() {
-		ObservableList<CreditCardTransaction> transactions = 
-				new ObservableListWrapper<CreditCardTransaction>(creditCardTransactionService.getAllCreditCardTransactions());
-		table.setItems(transactions);
+		List<CreditCardTransaction> transactions = creditCardTransactionService.getAllCreditCardTransactions();
+		table.setItems(FXCollections.observableList(transactions));
+		updateFields(transactions);
+	}
+
+	private void updateFields(List<CreditCardTransaction> transactions) {
+		totalItemsField.setText(String.valueOf(transactions.size()));
+		totalAmountField.setText(FormatterUtil.formatAmount(getTotalAmount(transactions)));
+	}
+
+	private static BigDecimal getTotalAmount(List<CreditCardTransaction> transactions) {
+		BigDecimal total = Constants.ZERO;
+		for (CreditCardTransaction transaction : transactions) {
+			total = total.add(transaction.getAmount());
+		}
+		return total;
 	}
 
 	@Override
@@ -121,8 +170,9 @@ public class CreditCardTransactionsListScreen extends StandardScreen {
 		CreditCardTransactionSearchCriteria criteria = 
 				filterCreditCardTransactionsDialog.getCreditCardTransactionSearchCriteria();
 		if (criteria != null) {
-			table.setItems(FXCollections.observableList(
-					creditCardTransactionService.searchCreditCardTransactions(criteria)));
+			List<CreditCardTransaction> transactions = creditCardTransactionService.searchCreditCardTransactions(criteria);
+			table.setItems(FXCollections.observableList(transactions));
+			updateFields(transactions);
 		}
 	}
 
